9cc125968ab7b7995d275f19a007d22feef9c370
Sam Gammon <sam@keen.io>
2013-11-26 19:46:37 -0800

    Added runscripts again - they work nicely with `python -m` syntax. Relates to CNTN-31.

---
+++ canteen/__main__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+  canteen runscript
+  ~~~~~~~~~~~~~~~~~
+
+  accepts calls to ``canteen`` as a module. can be run with
+  ``python -m canteen`` or ``python -m canteen/``, the latter
+  assuming you have it installed right next to you.
+
+  :author: Sam Gammon <sam@keen.io>
+  :copyright: (c) Keen IO, 2013
+  :license: This software makes use of the MIT Open Source License.
+            A copy of this library is included as ``LICENSE.md`` in
+            the root of the project.
+
+'''
---
+++ canteen/dispatch.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+  canteen dispatch
+  ~~~~~~~~~~~~~~~~
+
+  WSGI dispatch entrypoint. INCEPTION.
+
+  :author: Sam Gammon <sam@keen.io>
+  :copyright: (c) Keen IO, 2013
+  :license: This software makes use of the MIT Open Source License.
+            A copy of this library is included as ``LICENSE.md`` in
+            the root of the project.
+
+'''

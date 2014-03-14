7cceda2d200f307fdbdd30c56ff89240b045bca8
Sam Gammon <sam@keen.io>
2014-03-14 13:28:15 -0700

    Fixes for runtime/hook issues (for 'handler' hook) and session issues with data not making it into the session.

--- canteen/core/runtime.py
+++ canteen/core/runtime.py
@@ -246,7 +246,11 @@ class Runtime(object):
       self.execute_hooks('handler', **{
         'handler': flow,
         'environ': environ,
-        'start_response': start_response
+        'start_response': start_response,
+        'endpoint': endpoint,
+        'arguments': arguments,
+        'request': request,
+        'http': http
       })
 
       # dispatch time: INCEPTION.
@@ -317,6 +321,17 @@ class Runtime(object):
       _foreign_runtime_bridge.arguments = arguments
       _foreign_runtime_bridge.start_response = start_response
 
+      # call handler hooks
+      self.execute_hooks('handler', **{
+        'handler': handler,
+        'environ': environ,
+        'start_response': _foreign_runtime_bridge,
+        'endpoint': endpoint,
+        'arguments': arguments,
+        'request': request,
+        'http': http
+      })
+
       # initialize foreign handler with replaced start_response
       return handler(environ, _foreign_runtime_bridge)
 
@@ -335,6 +350,17 @@ class Runtime(object):
         ('Response', response.__class__)):
 
         handler.__globals__[prop] = val  # inject all the things
+
+      # call handler hooks
+      self.execute_hooks('handler', **{
+        'handler': handler,
+        'environ': environ,
+        'start_response': _foreign_runtime_bridge,
+        'endpoint': endpoint,
+        'arguments': arguments,
+        'request': request,
+        'http': http
+      })
 
       # call with arguments only
       result = handler(**arguments)

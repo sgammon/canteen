b7595af5b70e0b1f95a161989998c152fda26f3f
Sam Gammon <sam@momentum.io>
2014-09-13 05:12:46 -0700

    Fixing merge conflicts.

--- canteen/base/handler.py
+++ canteen/base/handler.py
@@ -335,11 +335,9 @@ class Handler(object):
     return self.__response__ if not direct else self
 
 
-<<<<<<< HEAD
-=======
-@decorators.configured
->>>>>>> Initial buildout for FLY-8 and FLY-5, which are being developed in tandem.
 class RealtimeHandler(Handler):
+
+  '''  '''
 
   def dispatch(self, **url_args):
 
